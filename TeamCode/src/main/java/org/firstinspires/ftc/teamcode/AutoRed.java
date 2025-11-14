6582f19f60c21655190c264e1f67d3c8559ee2b4
cpr-team <cougarpriderobotics-team@gmail.com>
2025-11-13 17:40:58 -0800

    WIP on setting velocity, testing shooter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.AutoFunctionsLinear;
+
+@Autonomous (name = "AutoRed")
+public class AutoRed extends AutoFunctionsLinear {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+
+        Integer direction = 1;
+        waitForStart();
+
+        drive_distance(2100);
+
+        rotate_degree(direction * 305);
+        drive_distance(-130);
+
+        shoot();
+        drive_distance(130);
+
+        rotate_degree(direction * 305);
+
+        //drive while intake
+        startIntake();
+        drive_distance(890);
+        stopIntake();
+
+        drive_distance(-890);
+
+        rotate_degree(direction * -305);
+        drive_distance(-100);
+
+        shoot();
+
+        // rotate_degree(2000);
+
+        // drive_distance(3200);
+
+        // rotate_degree(-950);
+
+        // startIntake();
+        // drive_distance(1470);
+        // stopIntake();
+
+        // drive_distance(-1470);
+
+        // rotate_degree(950);
+
+        // drive_distance(-3200);
+
+        // rotate_degree(-2000);
+
+        // shoot();
+
+        // rotate_degree(2500);
+
+        // drive_distance(4800);
+
+        // rotate_degree(-1450);
+
+        // startIntake();
+        // drive_distance(1800);
+        // stopIntake();
+
+        // drive_distance(-1800);
+
+        // rotate_degree(1450);
+
+        // drive_distance(-4800);
+
+        // rotate_degree(-2500);
+
+        // shoot();
+
+        // rotate_degree(-1050);
+
+        // drive_distance(-6600);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,175 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.Teleop;
+
+/**
+ *
+ * This file contains a minimal example of an iterative (Non-Linear) "OpMode". An OpMode is a
+ * 'program' that runs in either the autonomous or the TeleOp period of an FTC match. The names
+ * of OpModes appear on the menu of the FTC Driver Station. When an selection is made from the
+ * menu, the corresponding OpMode class is instantiated on the Robot Controller and executed.
+ *
+ * Remove the @Disabled annotation on the next line or two (if present) to add this OpMode to the
+ * Driver Station OpMode list, or add a @Disabled annotation to prevent this OpMode from being
+ * added to the Driver Station.
+ */
+@TeleOp
+public class Teleop extends OpMode {
+
+
+    protected DcMotor back_left;
+
+    protected DcMotor back_right;
+
+    protected DcMotor front_left;
+
+    protected DcMotor front_right;
+
+    protected DcMotor intake;
+
+    protected DcMotorEx intake2;
+
+    protected DcMotorEx shooter;
+    //private limelight3A limelight;
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init() {
+
+        back_left = hardwareMap.get(DcMotor.class, "back_left_motor") ;
+        back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        back_left.setDirection(DcMotor.Direction.REVERSE);
+
+        back_right = hardwareMap.get(DcMotor.class, "back_right_motor") ;
+        back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        front_left = hardwareMap.get(DcMotor.class, "front_left_motor") ;
+        front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        front_left.setDirection(DcMotor.Direction.REVERSE);
+
+        front_right = hardwareMap.get(DcMotor.class, "front_right_motor") ;
+        front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intake = hardwareMap.get(DcMotor.class, "intake") ;
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intake2 = hardwareMap.get(DcMotorEx.class, "intake2") ;
+        intake2.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        intake2.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter") ;
+        shooter.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        shooter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //limelight3A = limelight;
+    }
+
+    protected void drive(float back_left_power, float back_right_power, float front_left_power, float front_right_power) {
+        back_left.setPower(back_left_power);
+        back_right.setPower(back_right_power);
+        front_left.setPower(front_left_power);
+        front_right.setPower(front_right_power);
+    }
+
+
+    protected void startIntake() {
+        intake.setPower(-1);
+        intake2.setPower(-1);
+        //colorSense();
+    }
+
+
+    protected void stopIntake() {
+        intake.setPower(0);
+        intake2.setPower(0);
+    }
+
+    protected void reverseIntake() {
+        intake.setPower(1);
+        intake2.setPower(1);
+    }
+
+    protected void shoot() throws InterruptedException {
+        shooter.setVelocity(100);
+        intake2.setVelocity(100);
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        //color_sensor = hardwareMap.get(ColorSensor.class, "color_sensor");
+
+        float left_x = gamepad1.left_stick_x;
+        float left_y = -gamepad1.left_stick_y;
+        float right_x = gamepad1.right_stick_x;
+        boolean Intake = gamepad1.right_bumper;
+        float max = Math.max(Math.abs(left_y)+Math.abs(left_x)+Math.abs(right_x),1.0f);
+
+        float fr_drive = -(left_y - left_x - right_x)/max;
+        float fl_drive = -(left_y + left_x + right_x)/max;
+        float br_drive = (left_y + left_x - right_x)/max;
+        float bl_drive = -(left_y - left_x + right_x)/max;
+        drive(bl_drive, br_drive, fl_drive, fr_drive);
+
+        if (gamepad1.right_bumper){
+
+            startIntake();
+        }
+        else {
+            stopIntake();
+        }
+
+        if (gamepad1.left_bumper) {
+            try {
+                shoot();
+            }
+            catch (InterruptedException ie) {
+            }
+        }
+        else if (gamepad1.b){
+            reverseIntake();
+        }
+        if (gamepad1.y){
+            shooter.setVelocity(1000);
+            intake2.setVelocity(1000);
+        }
+        else if (gamepad1.x){
+            shooter.setPower(1);
+
+        }
+        else {
+            shooter.setPower(0);
+
+            intake2.setPower(0);
+        }
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+
+    }
+}
