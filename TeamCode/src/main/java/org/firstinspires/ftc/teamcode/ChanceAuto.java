a98ae3fdeefcb01c2a1b785db77c66807d39f321
Chance <Chance.hernandez09@gmail.com>
2026-09-08 17:18:53 -0700

    Fixed Confusing motor definintions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChanceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChanceAuto.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ChanceAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "ChanceAuto")
+public class ChanceAuto extends PracticeAFL{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        waitForStart();
+       drive_distance(5000);
+       sleep(5000);
+
+
+
+
+    }
 }
